07ec318cd92f49b5366d4d79845a2a4bdd997007
alvin-reyes <me@areyes.onl>
2023-03-13 22:10:46 -0400

    log/audit tracers

---
+++ db/database.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"github.com/application-research/delta-db/db_models"
+	_ "github.com/application-research/delta-db/db_models"
+	"github.com/application-research/delta-db/messaging"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func OpenDatabase(dbDsn string) (*gorm.DB, error) {
+	// use postgres
+	var DB *gorm.DB
+	var err error
+
+	if dbDsn[:8] == "postgres" {
+		DB, err = gorm.Open(postgres.Open(dbDsn), &gorm.Config{})
+	} else {
+		DB, err = gorm.Open(sqlite.Open(dbDsn), &gorm.Config{})
+	}
+
+	// generate new models.
+	ConfigureModels(DB) // create models.
+
+	if err != nil {
+		return nil, err
+	}
+	return DB, nil
+}
+
+func ConfigureModels(db *gorm.DB) {
+	db.AutoMigrate(&db_models.DeltaStartupLogs{}, &messaging.LogEvent{}, &db_models.ContentLog{}, &db_models.PieceCommitmentLog{}, &db_models.ContentDealLog{}, &db_models.ContentDealProposalLog{}, &db_models.ContentDealProposalParametersLog{}, &db_models.ContentWalletLog{}, &db_models.ContentMinerLog{}, &db_models.PieceCommitmentLog{}, &db_models.ContentWalletLog{}, &db_models.WalletLog{})
+}
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/application-research/delta-db/db_models"
 	"github.com/application-research/delta-db/messaging"
+	db2 "github.com/application-research/delta-events-consumer/db"
 	"github.com/nsqio/go-nsq"
+	"github.com/spf13/viper"
 	"log"
 	"os"
 	"os/signal"
@@ -14,24 +18,84 @@ import (
 func main() {
 
 	// Instantiate a new consumer that subscribes to the specified topic
-	//consumer := messaging.NewDeltaMetricsMessageConsumer()
-	config := nsq.NewConfig()
-	consumer, err := nsq.NewConsumer(messaging.PrimaryTopic, "default_channel", config)
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+	dbDsn := viper.Get("DBDSN").(string)
+	db, err := db2.OpenDatabase(dbDsn)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Instantiate a new consumer that subscribes to the specified topic
+	consumer := messaging.NewDeltaMetricsMessageConsumer()
 
 	// add a handler to consume messages
-	consumer.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
-		var content db_models.Content
-		json.Unmarshal(message.Body, &content)
-		log.Printf("Got a message: %v", content)
-		log.Printf("Got a message: %v", content.ID)
+	consumer.Consumer.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		var baseMessage messaging.DeltaMetricsBaseMessage
+		json.Unmarshal(message.Body, &baseMessage)
+
+		// parse the message body and check the db_models type using reflection
+		fmt.Println(baseMessage.ObjectType)
+		fmt.Println(baseMessage.Object)
+
+		switch baseMessage.ObjectType {
+		case "DeltaStartupLogs":
+			baseString := baseMessage.Object.(map[string]interface{})
+			var deltaStartupLogs db_models.DeltaStartupLogs
+			transcode(baseString, &deltaStartupLogs)
+			db.Create(&deltaStartupLogs)
+		case "ContentLog":
+			baseContentLog := baseMessage.Object.(map[string]interface{})
+			var contentLog db_models.ContentLog
+			transcode(baseContentLog, &contentLog)
+			db.Create(&contentLog)
+		case "ContentDealLog":
+			baseContentDealLog := baseMessage.Object.(map[string]interface{})
+			var contentDealLog db_models.ContentDealLog
+			transcode(baseContentDealLog, &contentDealLog)
+			db.Create(&contentDealLog)
+		case "ContentDealProposalLog":
+			baseContentDealLog := baseMessage.Object.(map[string]interface{})
+			var contentDealProposalLog db_models.ContentDealProposalLog
+			transcode(baseContentDealLog, &contentDealProposalLog)
+			db.Create(&contentDealProposalLog)
+		case "ContentDealProposalParametersLog":
+			baseContentDealLog := baseMessage.Object.(map[string]interface{})
+			var contentDealProposalParametersLog db_models.ContentDealProposalParametersLog
+			transcode(baseContentDealLog, &contentDealProposalParametersLog)
+			db.Create(&contentDealProposalParametersLog)
+		case "ContentMinerLog":
+			baseContentDealLog := baseMessage.Object.(map[string]interface{})
+			var contentMinerLog db_models.ContentMinerLog
+			transcode(baseContentDealLog, &contentMinerLog)
+			db.Create(&contentMinerLog)
+		case "ContentWalletLog":
+			baseContentDealLog := baseMessage.Object.(map[string]interface{})
+			var contentWalletLog db_models.ContentWalletLog
+			transcode(baseContentDealLog, &contentWalletLog)
+			db.Create(&contentWalletLog)
+		case "PieceCommitmentLog":
+			baseContentDealLog := baseMessage.Object.(map[string]interface{})
+			var pieceCommitmentLog db_models.PieceCommitmentLog
+			transcode(baseContentDealLog, &pieceCommitmentLog)
+			db.Create(&pieceCommitmentLog)
+		case "WalletLog":
+			baseContentDealLog := baseMessage.Object.(map[string]interface{})
+			var walletLog db_models.WalletLog
+			transcode(baseContentDealLog, &walletLog)
+			db.Create(&walletLog)
+		case "LogEvent":
+			baseLogEvent := baseMessage.Object.(map[string]interface{})
+			var logEvent messaging.LogEvent
+			transcode(baseLogEvent, &logEvent)
+			db.Create(&logEvent)
+		default:
+			log.Printf("Unknown message type: %v", baseMessage.ObjectType)
+		}
 
 		return nil
 	}))
 
-	err = consumer.ConnectToNSQD(messaging.MetricsTopicUrl)
+	err = consumer.Consumer.ConnectToNSQD(messaging.MetricsTopicUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,6 +107,12 @@ func main() {
 	<-sigChan
 
 	// disconnect
-	consumer.Stop()
+	consumer.Consumer.Stop()
 
+}
+
+func transcode(in, out interface{}) {
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(in)
+	json.NewDecoder(buf).Decode(out)
 }
